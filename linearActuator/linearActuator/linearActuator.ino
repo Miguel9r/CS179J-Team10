378ac3dbb259bd1cf20cb91bcfeb33432f6dc536
klabo001 <klabog001@gmail.com>
2020-04-22 23:39:08 -0700

    Debugged

--- linearActuator/linearActuator/linearActuator.ino
+++ linearActuator/linearActuator/linearActuator.ino
@@ -2,16 +2,17 @@
 
 Servo actuator;
 const int pwmPin = 9; //change to pwm pin
-
+int pwm = 1000;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   actuator.attach(pwmPin);
+  actuator.writeMicroseconds(1000);
+  delay(2000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
 
@@ -21,20 +22,28 @@ void serialEvent(){
     temp = Serial.read();
     switch (temp){
       case 0x01:
-        actuator.writeMicroseconds(2000); //2000 ms retracts arm
-        //delay(200);
-        actuator.writeMicroseconds(0);
-        Serial.println("expand received");
+        if(pwm > 1000){
+          pwm -= 5;
+        }
+        Serial.println("retract received");
+        break;
 
       case 0x02:
-        actuator.writeMicroseconds(1000); //1000 ms extends arm
-        //delay(200);
-        actuator.writeMicroseconds(0);
+        if(pwm < 2000){
+          pwm += 5;
+        }
         Serial.println("retract received");
+        break;
 
       default:
         Serial.println("unrecognized");
+        break;
     }
-    
+    actuator.writeMicroseconds(pwm);
+ //   delay(10);
+
   }
+//  while (Serial.available() > 0){
+//    temp = Serial.read();
+//  }
 }
--- linearActuator/linearActuatorProcessing/linearActuatorProcessing.pde
+++ linearActuator/linearActuatorProcessing/linearActuatorProcessing.pde
@@ -6,7 +6,7 @@ byte rawBytes[];
  String s;
 void setup(){
   size(500,500);
-  mySerial = new Serial(this, "COM3", 9600); //change COM to appropriate COM
+  mySerial = new Serial(this, "COM7", 9600); //change COM to appropriate COM
   textSize(30);
   s = "Press a for retract\nPress s for extend";
 }
@@ -23,16 +23,17 @@ void keyPressed(){
       mySerial.write(0x01);
       println("Sending retract signal");
       break;
-    
+
     case 's':
       mySerial.write(0x02);
       println("sending extend signal");
       break;
-    
+
     default:
       println("Unknown Command: "+key);
       break;
   }
+  //delay(150);
 }
 
 void keyReleased(){
@@ -42,15 +43,14 @@ void keyReleased(){
       mySerial.write(0x00);
       println("Finish retract signal");
       break;
-    
+
     case 's':
       mySerial.write(0x00);
       println("Finish extend signal");
       break;
-    
+
     default:
       println("Unknown Command: "+key);
       break;
   }
-  
 }
